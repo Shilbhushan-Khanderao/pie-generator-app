32d875b16be474f959f96291e7c50167ae60518e
Shilbhushan-Khanderao <kshilbhushan@gmail.com>
2023-10-10 19:51:44 +0530

    Revert "Update InputComponent.js with new subjects"
    This reverts commit 0ad9adef502eee1ca5d3a03ce4f5dda86b5d15d5.

--- src/component/InputComponent.js
+++ src/component/InputComponent.js
@@ -34,9 +34,7 @@ export const InputComponent = () => {
 
   //Overall Modules
   const moduleNameList = [
-    { value: "Logic Building Session", label: "LBS" },
-    { value: "Operating Systems", label: "OS" },
-    { value: "C++", label: "CPP" },
+    { value: "Concept of Programming & Operating Systems", label: "CPOS" },
     { value: "Object Oriented Programming with Java", label: "OOPJ" },
     { value: "Algorithm & Data Structures", label: "ADS" },
     { value: "Database Technologies", label: "DBT" },
